09a78c031c5005d42d5339f8c5d8a4653c912b2e
Anson Lee <a.lee3213@hotmail.com>
2020-03-11 16:34:14 +1100

    Allow clients to connect and send simple messages to the server

--- users.c
+++ users.c
@@ -3,6 +3,8 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
 
 /*
  *  Non-library Includes
@@ -17,5 +19,13 @@
 void handle_user(struct user *user) {
     extern int n_users;
     n_users++;
-    printf("n_users = %d\n", n_users);
+    //printf("n_users = %d\n", n_users);
+    //printf("id = %d\n", user->id);
+    //printf("name = %s\n", user->name);
+
+    char buffer[500] = {0};
+    while (read(user->conn_fd, buffer, 500) > 0) {
+        printf("%s: %s\n", user->name, buffer);
+        bzero(buffer, 500);
+    }
 }
