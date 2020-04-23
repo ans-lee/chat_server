2d35c9c8ebe669e2b81540c65b76773ce196d7f6
Anson Lee <a.lee3213@hotmail.com>
2020-04-23 18:54:17 +1000

    Allow the server to check for different ports and create a config file

--- chat_server.c
+++ chat_server.c
@@ -11,11 +11,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <arpa/inet.h>
 #include <pthread.h>
+#include <errno.h>
 
 /*
  *  Non-library Includes
@@ -28,7 +25,8 @@
  *  Function Prototypes
  */
 
-static void get_server_ip_address(char *ip_buffer);
+static void check_for_config_file();
+static void create_config_file();
 static void setup_server(int *server_fd, struct sockaddr_in *server_address);
 static void handle_connections(int server_fd);
 
@@ -38,15 +36,14 @@ static void handle_connections(int server_fd);
 
 int main(void) {
     initialise_users_mutex();
+    check_for_config_file();
 
     int server_fd;
     struct sockaddr_in server_address = {0};
-    char server_ip_address[MAX_IPV4_LEN + 1];   // +1 for null-terminating byte
 
-    get_server_ip_address(server_ip_address);
     setup_server(&server_fd, &server_address);
-    printf("######### Chat Server started on IP %s, Port %d\n\n",
-            server_ip_address, ntohs(server_address.sin_port));
+    printf("######### Chat Server started on Port %d, listening on all Network Interfaces\n\n",
+            ntohs(server_address.sin_port));
     printf("--------------------\n");
     printf("          Server Log\n");
     printf("--------------------\n");
@@ -62,20 +59,56 @@ int main(void) {
  *  Functions
  */
 
-// Retrieves the IPv4 IP address of this machine, which is the server's IP address
-static void get_server_ip_address(char *ip_buffer) {
-    int fd = socket(AF_INET, SOCK_DGRAM, 0);
-    struct ifreq ifr = {0};
+// Check for configuration file, if missing create one
+static void check_for_config_file() {
+    if (access(CONFIG_NAME, F_OK) == -1) {
+        printf("Creating config file %s...\n", CONFIG_NAME);
+        create_config_file();
+    }
 
-    // Configure ifreq struct options
-    ifr.ifr_addr.sa_family = AF_INET;
-    strncpy(ifr.ifr_name, DEFAULT_NET_INTERFACE_NAME, IFNAMSIZ - 1);
-
-    ioctl(fd, SIOCGIFADDR, &ifr);
-    close(fd);
-
-    sprintf(ip_buffer, "%s", inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->sin_addr));
+    if (access(CONFIG_NAME, R_OK) == -1) {
+        fprintf(stderr, "error: %s must have read permissions", CONFIG_NAME);
+        exit(EXIT_FAILURE);
+    }
 }
+
+// Creates the configuration file
+static void create_config_file() {
+    // Create config file
+    FILE *conf = fopen(CONFIG_NAME, "w");
+    if (conf == NULL) {
+        perror("fopen:");
+        exit(EXIT_FAILURE);
+    }
+
+    // Write all text to the config file
+
+    // Title comment
+    fprintf(conf, "# Simple configuration file for server. Any line with # at the front will be ignored.\n\n");
+
+    // Ports option
+    fprintf(conf, "# Change ports here to adjust which ports the server should\n"
+                  "# use, separated by ONE comma. DO NOT put anything after \"ports:\",\n"
+                  "# change the numbers and use numbers greater than 0 and less than 65535 ONLY!\n"
+                  "# e.g.\n"
+                  "# ports:8080,7080,6080\n"
+                  "# NOTE: ports below 2000 are usually reserved. Ports above 5000 should be fine.\n"
+                  "ports:8080,7080,6080\n\n");
+
+    fclose(conf);
+}
+
+// Read from the config file and set some variables
+// TODO
+/*
+static void read_from_config_file() {
+    FILE *conf = fopen(CONFIG_NAME, "r");
+    if (conf == NULL) {
+        perror("fopen:");
+        exit(EXIT_FAILURE);
+    }
+}
+*/
 
 // Initialises the settings of the server and starts it up
 static void setup_server(int *server_fd, struct sockaddr_in *server_address) {
@@ -88,12 +121,26 @@ static void setup_server(int *server_fd, struct sockaddr_in *server_address) {
     // Setup the socket
     server_address->sin_family = AF_INET;
     server_address->sin_addr.s_addr = INADDR_ANY;
-    server_address->sin_port = htons(DEFAULT_PORT);
 
-    // Assign address to the socket
-    if (bind(*server_fd, (struct sockaddr*) server_address, sizeof(*server_address)) == -1) {
-        perror("bind");
-        exit(EXIT_FAILURE);
+    // Pick an available port
+    int serv_ports[] = {SERV_PORT_1, SERV_PORT_2, SERV_PORT_3};
+    int size = sizeof(serv_ports) / sizeof(serv_ports[0]);
+    for (int i = 0; i < size; i++) {
+        // Set the port
+        server_address->sin_port = htons(serv_ports[i]);
+
+        // Assign address to the socket
+        if (bind(*server_fd, (struct sockaddr*) server_address, sizeof(*server_address)) == 0) {
+            break;
+        } else if (i == size - 1) {
+            extern int errno;
+            if (errno == EADDRINUSE) {
+                fprintf(stderr, "bind: all available ports are occupied\n");
+            } else {
+                perror("bind");
+            }
+            exit(EXIT_FAILURE);
+        }
     }
 
     // Tell socket to accept incoming connections
--- chat_server.h
+++ chat_server.h
@@ -4,12 +4,29 @@
 //
 
 /*
+ *  Enums
+ */
+
+// Ports that the server can run on if not occupied
+enum server_ports {
+    SERV_PORT_1 = 8080,
+    SERV_PORT_2 = 7080,
+    SERV_PORT_3 = 6080
+};
+
+/*
  *  #defines
  */
 
-#define MAX_IPV4_LEN                    15          // Max length of an IPv4 ip address
-#define DEFAULT_NET_INTERFACE_NAME      "eth0"      // Default network interface name to run the server on
-#define DEFAULT_PORT                    8080        // Setting this to 0 will pick a random port
-#define MAX_CONNS                       10          // Max queing connections allowed
-#define MAX_USERS                       20          // Max users allowed on the server at once
-#define MSG_MAX                         1000        // 1000 character limit for messages
+#define CONFIG_NAME                     "config.txt"    // Configuration file name
+#define MAX_CONNS                       10              // Max queing connections allowed
+#define MAX_USERS                       20              // Max users allowed on the server at once
+#define MSG_MAX                         1000            // 1000 character limit for messages
+
+/*
+ *  Structs
+ */
+
+struct serv_settings {
+    int *ports;     // Array of Port numbers
+};
